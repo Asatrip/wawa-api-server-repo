0fea2a15cec4af3e89c487279e5597a0426e0699
atrip <greatwany7@gmail.com>
2022-11-12 16:33:50 +0900

    Add root path route hadler

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const { Member } = db;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("url should contain /api/..");
+});
+
 app.get("/api/members", async (req, res) => {
   const { team } = req.query;
   if (team) {
--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 
-const { username, password, database, host, dialect } = config.development;
+const { username, password, database, host, dialect } = config;
 const sequelize = new Sequelize(database, username, password, {
   host,
   dialect,
